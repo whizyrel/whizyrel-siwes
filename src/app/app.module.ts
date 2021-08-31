5b9eb09c3f5c56b26c2d195d1e1d27db975eeed6
Israel Oluwole Olalekan <olaleyeisrael@gmail.com>
2021-08-31 13:58:45 +0100

    feat: dashboard initial design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatRippleModule } from '@angular/material/core';
+import { NgApexchartsModule } from 'ng-apexcharts';
 
 import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LandingComponent
-  ],
+  declarations: [AppComponent, LandingComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatRippleModule,
+    NgApexchartsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -6,10 +6,86 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing.component.scss']
 })
 export class LandingComponent implements OnInit {
+  public active = 0;
+  public cities = ['Lagos', 'Ogun', 'Abuja', 'Oyo', 'Enugu', 'Anambra', 'Akwa Ibom', 'Nasarawa'];
+  public today = new Date();
+  public options: any = {
+    chart: {
+      height: 360,
+      type: 'area',
+      toolbar: {show: false},
+      redrawOnParentResize: true,
+      redrawOnWindowResize: true,
+    },
+    stroke: {
+      width: 2,
+      curve: 'smooth',
+      lineCap: 'butt',
+    },
+    dataLabels: {
+      enabled: false
+    },
+    series: [
+      {
+        name: 'Series 1',
+        data: [45, 56, 50, 53, 48, 45, 43]
+      }
+    ],
+    grid: {show: false},
+    fill: {
+      type: 'gradient',
+      gradient: {
+        shadeIntensity: 1,
+        opacityFrom: 1,
+        opacityTo: 0.5,
+        stops: [0, 90, 100]
+      },
+      colors: ['#3f51b5'],
+    },
+    colors: ['#3f51b5'],
+    xaxis: {
+      labels: {
+        style: {
+          cssClass: 'tspan__label',
+        }
+      },
+      axisTicks: {show: false},
+      axisBorder: {color: '#3f51b5'},
+      categories: [
+        '01 Jan',
+        '02 Jan',
+        '03 Jan',
+        '04 Jan',
+        '05 Jan',
+        '06 Jan',
+        '07 Jan'
+      ],
+    },
+    yaxis: {
+      show: false,
+    },
+    markers: {
+      colors: 'white',
+      strokeColors: '#3f51b5',
+      shape: 'circle',
+    },
+    tooltip: {
+      custom: ({series, seriesIndex, dataPointIndex}: any): any => {
+        return `
+                <div class="mat-caption custom-tooltip rounded-pill px-3 py-1">
+                  <span> ${series[seriesIndex][dataPointIndex]}&deg;C</span>
+                </div>
+          `;
+      }
+    }
+  };
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public getDate(): Date {
+    return new Date();
+  }
 }
